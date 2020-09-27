2b6bd2d2e5318dbb9ea171295030ef4fa57da535
hektor <hektor@pop-os.localdomain>
2020-09-27 06:58:54 +0200

    fixed some stuff

--- README.md
+++ README.md
@@ -1,31 +1,36 @@
-# cub3d_destroyer - Let's Break Your Parser!
+# Cub3d_destroyer - Let's Break Your Parser!
 
-cub3d_destroyer tries to break your cub3d parser with almost 300 different .cub files.
-Get ready... only the best ones will survive!
+Cub3d_destroyer tries to break your cub3d parser with almost 300 different .cub files [MANDATORY PART ONLY].
+The main goal of this project is to offer 42 students a tool which detects as many parsing bugs as possible for this project.
+It is also an easy way to get a feeling on how your code behaves and where you can improve it.
+
 
 ## Disclaimer
 The 42 subject lays out a big set of rules for what should be considered as valid/invalid input.
 Some of these rules offer a great scope for interpretation.
-While testing, I strongly advise you to take a close look on the .cub files and to be always open for discussion.
+While testing, I strongly advise you to take a close look on the .cub files, read the subject carefully, make up your own mind and to be always open for discussion :)!
 
 
-## Requirements to make this test run
+## 	Requirements to make this test run
 - Your executable should be named "cub3D". With a capital "D" just like it is stated in the subject.
-- Your program/parser has to print out "Error\n" after encountering a misconfiguration in the .cub file.
+- Your program has to print out "Error\n" after encountering a misconfiguration in the .cub file.
 
 
-## Step-by-Step Guide
-1. Use this command to clone the repository to the root directory of your cub3d project.
+##	Step-by-Step Guide
+1.	Use this command to clone the repository to the root directory of your cub3d project.
 ```
 git clone https://github.com/Dustin42Codam/cub3d_destroyer.git
 ```
-2. Execute the shell script
+2.	cd into the cloned repo and execute the shell script
 ```
-bash destroyer.sh
+cd cub3d_destroyer && bash destroyer.sh
 ```
-3. Wait for the results!
-   When your parser will fail, you will see the results directly in the terminal.
-   All your failed parsing attempts will be also stored inside a logfile ("damage_report.log").
+3.	Wait for the results!
+	If the parser fails to interpret a map accordingly, the failed case will be directly printed out to stdout.
+	All failed parsing attempts are also stored inside a logfile (look at screenshots below).
+
+4.	Just add your own .cub files to the test process! It is super simple. Add all your as invalid considered files
+	to the folder "invalid_maps" and the valid stuff goes of course to "valid_maps".
 
 
 # Screenshots
--- destroyer.sh
+++ destroyer.sh
@@ -6,7 +6,7 @@
 #    By: dkrecisz <dkrecisz@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/07/06 04:44:53 by dkrecisz      #+#    #+#                  #
-#    Updated: 2020/09/27 00:50:17 by dkrecisz      ########   odam.nl          #
+#    Updated: 2020/09/27 06:56:23 by dkrecisz      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -55,7 +55,10 @@ printf "\n\t${BYEL}+------------------------------------+"
 printf "\n\t${BYEL}+====================================+\n${RESET}"
 
 #Try to make cub3D
-make -C ../ 
+printf "\n${BHWHT}Attempting to make cub3D in parent directory...\n"
+echo "$ make -C ../ &>/dev/null" && make -C ../ &>/dev/null
+
+#Exit if make failed
 if [[ $? -ne 0 ]]; then
 	printf "\n${BYEL}${REDB} ${BHWHT} Error: Failed to make cub3D in parent directory! ${RESET}${BYEL}\n${RESET}"
 	exit 1
@@ -67,29 +70,31 @@ OK=0
 
 #LOGFILE FOR PARSER DESTROYED STUFF
 log=damage_report.log
-errorMsg=error.txt
+out=tmp.txt
 date > $log && printf "\n\n%sCUB3D DESTROYER - DAMAGE REPORT%s\n\n" $div2 $div2 >> $log && echo >> $log
 
 
 printf "\n${BYEL}+==================${REDB} ${BHWHT}START TESTING ${RESET}${BYEL}==================+\n${RESET}"
 
-#TEST INVALID MAPS
-for file in ./invalid_maps/invalid_*.cub
+#ITERATE THROUGH INVALID MAPS
+for file in ./invalid_maps/*.cub
 do
-	../cub3D $file &>$errorMsg &
-	sleep 0.06
-	killall cub3D &>/dev/null
-	grep -q "Error$" $errorMsg
+	rm -f $out
+	../cub3D $file &>$out &
+	sleep 0.02
+	while [ ! -f $out ]; do sleep 0.001; done
+	killall -q cub3D
+	grep -q "Error$" $out
 	if [[ $? -ne 0 ]]; then
 		FAIL=$((FAIL+1))
 		printf "${REDB}${BHYEL}MAP: %-42s${RESET}%s${BRED}[DESTROYED]\n" $file " " && printf ${RESET}
 		printf "${REDB}${BWHT}%s${REDB}${BHYEL} FILE CONTENTS ${REDB}${BWHT}%s${RESET}\n" $div1 $div1
 		printf "%s[DESTROYED STUFF #%2.d]%s\n" $div1 $FAIL $div1 >> $log && ls $file >> $log && cat $file >> $log && printf "\n\n" >> $log
 		printf "PARSER OUTPUT:\n" >> $log
-		cat $errorMsg >> $log
+		cat $out >> $log
 		cat $file
 		printf "\n${BYEL}%s${REDB}${BHYEL} PARSER OUTPUT ${RESET}${BYEL}%s${RESET}\n" $div1 $div1
-		cat $errorMsg
+		cat $out
 		printf "${REDB}${BWHT}%s${YELHB}${BRED} CONTINUE TEST ${REDB}${BWHT}%s${RESET}\n" $div1 $div1
 		echo
 	else
@@ -98,23 +103,27 @@ do
 	fi
 done
 
-#TEST VALID MAPS
-for file in valid_maps/valid_*.cub
+
+#ITERATE THROUGH VALID MAPS
+for file in valid_maps/*.cub
 do
-	../cub3D $file &>$errorMsg &
-	sleep 0.06
-	killall cub3D &>/dev/null
-	grep -q "Error$" $errorMsg
+	rm -f $out
+	../cub3D $file &>$out &
+	sleep 0.02
+	while [ ! -f $out ]; do sleep 0.001; done
+	kill $!
+	wait $! 2>/dev/null
+	grep -q "Error$" $out
 	if [[ $? -eq 0 ]]; then
 		FAIL=$((FAIL+1))
 		printf "${REDB}${BHYEL}MAP: %-42s${RESET}%s${BRED}[DESTROYED]\n" $file " " && printf ${RESET}
 		printf "${REDB}${BWHT}%s${REDB}${BHYEL} FILE CONTENTS ${REDB}${BWHT}%s${RESET}\n" $div1 $div1
 		printf "%s[DESTROYED STUFF #%2.d]%s\n" $div1 $FAIL $div1 >> $log && ls $file >> $log && cat $file >> $log && printf "\n\n" >> $log
 		printf "PARSER OUTPUT:\n" >> $log
-		cat $errorMsg >> $log
+		cat $out >> $log
 		cat $file
 		printf "\n${BYEL}%s${REDB}${BHYEL} PARSER OUTPUT ${RESET}${BYEL}%s${RESET}\n" $div1 $div1
-		cat $errorMsg
+		cat $out
 		printf "${REDB}${BWHT}%s${YELHB}${BRED} CONTINUE TEST ${REDB}${BWHT}%s${RESET}\n" $div1 $div1
 		echo
 	else
@@ -123,12 +132,12 @@ do
 	fi
 done
 
-rm $errorMsg
+rm -f $out
 
 #OUTPUT FINAL RESULT
 printf "\n\n${BBLU}%s${BWHT} FINAL RESULT ${BBLU}%s\n\n" $div1 $div1
-printf "${BGRN}PASSED AND BULLETPROOF:\t%d\t[GREAT JOB MATE]\n" $OK
-printf "${BRED}FAILED AND DESTROYED:\t%d \t[RIP]\n\n" $FAIL
+printf "${BGRN}BULLETPROOF:\t%d\t\n" $OK
+printf "${BRED}DESTROYED:\t%d \t\n\n" $FAIL
 printf "${BBLU}================== ${BWHT} DAMAGE_REPORT.LOG CREATED ${BBLU} ===================${RESET}\n\n"
 
 exit 0
